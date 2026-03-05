3375b7298f50a6bca5d45b12565ba1ca9ca81def
afsana-jannat <afsanajannat205@gmail.com>
2026-03-05 10:33:38 +0600

    banner design done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -12,13 +12,25 @@ const Banner = () => {
                     <h2>In-Progress</h2>
                     <h2>0</h2>
                 </div>
+
+                <img src="/public/vic2.jpeg" alt="" className="absolute bottom-0 right-0 opacity-30 mix-blend-screen" />
+
             </div>
-            <div className="rounded-md w-6xl h-60 
-            bg-linear-to-t from-[#54CF68] to-[#00827A] to-90%
-            text-white  flex flex-col items-center justify-center">
-                <h2>Resolved</h2>
-                <h2>0</h2>
+            <div className="relative rounded-md w-6xl h-60 
+            bg-gradient-to-t from-[#54CF68] to-[#00827A] to-90% overflow-hidden
+            ">
+
+                <img src="/public/vector1.png" alt="" className="absolute bottom-0 left-0 opacity-40" />
+
+                <div className="text-white absolute inset-0 flex flex-col items-center justify-center">
+                    <h2>Resolved</h2>
+                    <h2>0</h2>
+                </div>
+
+                <img src="/public/vic2.jpeg" alt="" className="absolute bottom-0 right-0 opacity-30 mix-blend-screen" />
+
             </div>
+
         </div>
     );
 };
